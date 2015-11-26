ace4a1d0e9256d26e469e446d6e606138829a575
Henrik <henrik_mathiesen@hotmail.com>
2015-11-26 20:39:26 +0100

    -grunt jobs -changed routing to static files

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+/* global grunt */
+
+module.exports = function (grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+	var jsBundle = {
+		'./app/js/bld/lib.js': [
+			'./bower_components/angular/angular.js',
+			'./bower_components/angular-route/angular-route.js'
+		],
+		'./app/js/bld/app.js': [
+			'./app/js/src/modules/angularHtml5Routing.js'
+		]
+	}
+
+	grunt.initConfig({
+		clean: ['./app/js/bld'],
+		uglify: {
+			dev: {
+				files: jsBundle,
+				options: { sourceMap: true }
+			},
+			prod: {
+				files: jsBundle,
+				options: { compress: { pure_funcs: ['console.log'] } }
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['./app/js/src/**/*.js'],
+				tasks: ['dev']
+			}
+		}
+	});
+
+	grunt.registerTask('dev', ['clean', 'uglify:dev']);
+	grunt.registerTask('prod', ['clean', 'uglify:prod']);
+	grunt.registerTask('default', ['dev', 'watch']);
+
+};
--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var app = express();
 
 //
 // Set access to static files; virtual path and actual path
-app.use('/lib',  express.static(__dirname + '/bower_components'));
-app.use('/app',  express.static(__dirname + '/app/js/bld'));
-app.use('/app',  express.static(__dirname + '/app/sass/bld'));
-app.use('/app',  express.static(__dirname + '/app/views'));
+app.use('/',  express.static(__dirname + '/app/js/bld'));
+app.use('/',  express.static(__dirname + '/app/sass/bld'));
+app.use('/',  express.static(__dirname + '/app/views'));
 
 //
 // Serve the index.html for all routes (front end routing will take it from there)
