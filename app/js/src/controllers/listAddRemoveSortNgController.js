8acc16478436210785e249950dcb319c775efc3f
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2015-12-08 14:18:04 +0100

    -add,remove,sort with ng

--- app/js/src/controllers/listAddRemoveSortNgController.js
+++ app/js/src/controllers/listAddRemoveSortNgController.js
@@ -1,36 +1,46 @@
-(function(){
-	
-	var listAddRemoveSortNgController = function($scope){
+(function () {
+
+	var listAddRemoveSortNgController = function ($scope) {
 		console.log("listAddRemoveSortNgController");
-		
+
 		$scope.names = ['Adam', 'Bertil', 'Ceasar'];
 		$scope.newName = "";
+
+
+		var swapArrayElements = function (indexA, indexB) {
+			var temp = $scope.names[indexA];
+			$scope.names[indexA] = $scope.names[indexB];
+			$scope.names[indexB] = temp;
+		};
+		
 		
 		// Add
-		$scope.addName = function(){
-			if(!$scope.newName) { return; }
+		$scope.addName = function () {
+			if (!$scope.newName) { return; }
 			$scope.names.push($scope.newName);
 			$scope.newName = "";
 		};
 		
 		// Remove
-		$scope.removeName = function(index){
+		$scope.removeName = function (index) {
 			$scope.names.splice(index, 1);
 		};
 		
 		// Sort, up
-		$scope.sortUp = function(index){
-			if(index < 1) { return; }
+		$scope.sortUp = function (index) {
+			if (index < 1) { return; }
+			swapArrayElements(index, index - 1);
 		};
 		
 		// Sort, down
-		$scope.sortDown = function(index){
-			if(index >= $scope.names.length - 1) { return; }
+		$scope.sortDown = function (index) {
+			if (index >= $scope.names.length - 1) { return; }
+			swapArrayElements(index, index + 1);
 		};
-		
+
 	};
-	
+
 	angular.module('angularHtml5Routing').controller('listAddRemoveSortNgController', listAddRemoveSortNgController);
 	listAddRemoveSortNgController.$inject = ['$scope'];
-	
+
 })();
