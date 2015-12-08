86a9e512ee56aa623745b7202980351cbe615ea6
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2015-12-08 13:20:50 +0100

    -Add, Remove, Sort NG started

--- app/js/src/controllers/listAddRemoveSortNgController.js
+++ app/js/src/controllers/listAddRemoveSortNgController.js
@@ -2,6 +2,32 @@
 	
 	var listAddRemoveSortNgController = function($scope){
 		console.log("listAddRemoveSortNgController");
+		
+		$scope.names = ['Adam', 'Bertil', 'Ceasar'];
+		$scope.newName = "";
+		
+		// Add
+		$scope.addName = function(){
+			if(!$scope.newName) { return; }
+			$scope.names.push($scope.newName);
+			$scope.newName = "";
+		};
+		
+		// Remove
+		$scope.removeName = function(index){
+			$scope.names.splice(index, 1);
+		};
+		
+		// Sort, up
+		$scope.sortUp = function(index){
+			if(index < 1) { return; }
+		};
+		
+		// Sort, down
+		$scope.sortDown = function(index){
+			if(index >= $scope.names.length - 1) { return; }
+		};
+		
 	};
 	
 	angular.module('angularHtml5Routing').controller('listAddRemoveSortNgController', listAddRemoveSortNgController);
